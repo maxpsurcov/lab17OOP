8b564dc20de84552003d9ad06f0d14c230789555
maxpsurcov <max.psurcov1@gmail.com>
2023-04-06 14:46:12 +0300

    Добавьте файлы проекта.

---
+++ lab17OOP/Pair.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab17OOP
+{
+    using System;
+
+    abstract class Pair
+    {
+        public abstract Pair Add(Pair p);
+        public abstract Pair Subtract(Pair p);
+        public abstract Pair Multiply(Pair p);
+        public abstract Pair Divide(Pair p);
+    }
+
+    class FazzyNumber : Pair
+    {
+        private double value;
+        private double error;
+
+        public FazzyNumber(double value, double error)
+        {
+            this.value = value;
+            this.error = error;
+        }
+
+        public override Pair Add(Pair p)
+        {
+            FazzyNumber num = (FazzyNumber)p;
+            return new FazzyNumber(this.value + num.value, this.error + num.error);
+        }
+
+        public override Pair Subtract(Pair p)
+        {
+            FazzyNumber num = (FazzyNumber)p;
+            return new FazzyNumber(this.value - num.value, this.error + num.error);
+        }
+
+        public override Pair Multiply(Pair p)
+        {
+            FazzyNumber num = (FazzyNumber)p;
+            double newVal = this.value * num.value;
+            double newErr = (this.error * num.value) + (num.error * this.value);
+            return new FazzyNumber(newVal, newErr);
+        }
+
+        public override Pair Divide(Pair p)
+        {
+            FazzyNumber num = (FazzyNumber)p;
+            double newVal = this.value / num.value;
+            double newErr = (this.error / num.value) + (num.error * this.value / (num.value * num.value));
+            return new FazzyNumber(newVal, newErr);
+        }
+
+        public override string ToString()
+        {
+            return $"({value}±{error})";
+        }
+    }
+
+    class Fraction : Pair
+    {
+        private int numerator;
+        private uint denominator;
+
+        public Fraction(int numerator, uint denominator)
+        {
+            this.numerator = numerator;
+            this.denominator = denominator;
+        }
+
+        public override Pair Add(Pair p)
+        {
+            Fraction frac = (Fraction)p;
+            uint lcm = LCM(this.denominator, frac.denominator);
+            int newNum = (int)(this.numerator * lcm / this.denominator) + (int)(frac.numerator * lcm / frac.denominator);
+            return new Fraction(newNum, lcm);
+        }
+
+        public override Pair Subtract(Pair p)
+        {
+            Fraction frac = (Fraction)p;
+            uint lcm = LCM(this.denominator, frac.denominator);
+            int newNum = (int)(this.numerator * lcm / this.denominator) - (int)(frac.numerator * lcm / frac.denominator);
+            return new Fraction(newNum, lcm);
+        }
+
+        public override Pair Multiply(Pair p)
+        {
+            Fraction frac = (Fraction)p;
+            int newNum = this.numerator * frac.numerator;
+            uint newDen = this.denominator * frac.denominator;
+            return new Fraction(newNum, newDen);
+        }
+
+        public override Pair Divide(Pair p)
+        {
+            Fraction frac = (Fraction)p;
+            int newNum = this.numerator * (int)frac.denominator;
+            uint newDen = this.denominator * (uint)Math.Abs(frac.numerator);
+            if (frac.numerator < 0)
+            {
+                newNum = -newNum;
+            }
+            return new Fraction(newNum, newDen);
+        }
+
+        private static uint GCD(uint a, uint b)
+        {
+            while (b != 0)
+            {
+                uint r = a % b;
+                a = b;
+                b = r;
+            }
+            return a;
+        }
+
+        private static uint LCM(uint a, uint b)
+        {
+            return (a * b) / GCD(a, b);
+        }
+
+        public override string ToString()
+        {
+            return $"{numerator}/{denominator}";
+        }
+    }
+}
---
+++ lab17OOP/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab17OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pair pair1 = new FazzyNumber(10, 1);
+            Pair pair2 = new FazzyNumber(20, 2);
+            Console.WriteLine($"pair1 = {pair1}");
+            Console.WriteLine($"pair2 = {pair2}");
+            Console.WriteLine($"pair1 + pair2 = {pair1.Add(pair2)}");
+            Console.WriteLine($"pair1 - pair2 = {pair1.Subtract(pair2)}");
+            Console.WriteLine($"pair1 * pair2 = {pair1.Multiply(pair2)}");
+            Console.WriteLine($"pair1 / pair2 = {pair1.Divide(pair2)}");
+            Pair pair3 = new Fraction(2, 3);
+            Pair pair4 = new Fraction(-1, 4);
+            Console.WriteLine($"pair3 = {pair3}");
+            Console.WriteLine($"pair4 = {pair4}");
+            Console.WriteLine($"pair3 + pair4 = {pair3.Add(pair4)}");
+            Console.WriteLine($"pair3 - pair4 = {pair3.Subtract(pair4)}");
+            Console.WriteLine($"pair3 * pair4 = {pair3.Multiply(pair4)}");
+            Console.WriteLine($"pair3 / pair4 = {pair3.Divide(pair4)}");
+            Console.ReadKey();
+        }
+
+    }
+
+}
